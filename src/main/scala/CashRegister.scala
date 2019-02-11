48cc1c2bea594f9b32c3d48e8fd1ef3ee1270667
Brendan Straughan <brendan.straughan@digital.hmrc.gov.uk>
2019-02-11 14:29:13 +0000

    BrendansBranch

--- src/main/scala/CashRegister.scala
+++ src/main/scala/CashRegister.scala
@@ -1,8 +1,8 @@
 class CashRegister(val currentTotal : Double, offers : List[StockItem => Double]) {
 
-  def scan(item : StockItem) : CashRegister = {
-   val discountedPrice = (for(singleOffer <- offers) yield singleOffer(item)).min
-    new CashRegister(currentTotal + discountedPrice, offers)
+  def scan (item : StockItem) : CashRegister = {
+
+    new CashRegister(currentTotal + item.normalPrice,offers)
   }
 
 }
